a5fd7179f1f099f5899d3414ee62ae8bf85c806f
Bernhard Richter <bernhard.richter@gmail.com>
2023-12-26 20:08:53 +0100

    Fixed build

--- output/server/chunks/integer-2c1bdfc8.js
+++
@@ -1,6 +0,0 @@
-function match(param) {
-  return /^\d+$/.test(param);
-}
-
-export { match };
-//# sourceMappingURL=integer-2c1bdfc8.js.map
--- output/server/chunks/stores2-2f49b5e8.js
+++
@@ -1,6 +0,0 @@
-import { w as writable } from './index-b3363d5c.js';
-
-const currentUser = writable();
-
-export { currentUser as c };
-//# sourceMappingURL=stores2-2f49b5e8.js.map
