5a7298111165372a25f11e39b63749a3efd8d242
Bernhard Richter <bernhard.richter@gmail.com>
2024-02-12 23:08:38 +0100

    Use links in tabs

--- src/routes/locations/[locationId=integer]/+layout.svelte
+++ src/routes/locations/[locationId=integer]/+layout.svelte
@@ -1,29 +1,46 @@
 <script lang="ts">
 	import { CenteredHeader } from '$lib/components';
 	import type { LayoutData } from './$types';
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 	import { page } from '$app/stores';
 	export let data: LayoutData;
-	//export const ssr = false;
 	let selectedTab = 0;
 	let selectedClass =
 		'inline-block p-3 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500';
 	let unselectedClass =
 		'inline-block p-3 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300';
+
+	const links = [
+		{ name: 'Home', path: `/locations/${data.location.id}` },
+		{ name: 'About', path: '/about' },
+		{ name: 'Contact', path: '/contact' }
+	];
 </script>
 
 <CenteredHeader>{data.location.name}</CenteredHeader>
 <ul
 	class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
 >
+	<!-- {#each links as link}
+		<li>
+			<a
+				href={link.path}
+				class={link.path === $page.url.pathname ? selectedClass : unselectedClass}
+				on:click={() => {
+					selectedTab = 0;
+				}}>{link.name}</a
+			>
+		</li>
+	{/each} -->
 	<li class="me-2">
 		<a
 			href="/locations/{data.location.id}"
-			aria-current="page"
-			class={selectedTab === 0 ? selectedClass : unselectedClass}
+			class={`/locations/${data.location.id}` === $page.url.pathname
+				? selectedClass
+				: unselectedClass}
 			on:click={() => {
 				selectedTab = 0;
 				console.log($page.url.pathname);
+				console.log(data.location.id);
 			}}>Details</a
 		>
 	</li>
